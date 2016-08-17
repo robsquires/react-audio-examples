f1d2970cb1b570eaff7947b9ac4f72299c6ad520
Rob Squires <rob.squires@gmail.com>
2016-08-17 17:00:01 +0100

    prepped-1 basic audio

--- src/Audio.js
+++ src/Audio.js
@@ -5,24 +5,9 @@ import { track1 } from './files'
 
 class Audio extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      src: track1,
-      playing: false // set this 'true' to play on component mount
-    }
-  }
-
-  componentDidMount() {
-    if (this.state.playing) {
-      ReactDOM.findDOMNode(this).play()
-    }
-  }
-
   render() {
     return (
-      <audio refs='audio' src={this.state.src}/>
+      <audio src={track1}/>
     )
   }
 }
