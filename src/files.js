7d16ca7759119d8eeeeb618e946f12754ea91c0a
Rob Squires <rob.squires.ext@bbc.co.uk>
2016-08-18 10:28:13 +0100

    updated tracks

--- src/files.js
+++ src/files.js
@@ -1,7 +1,12 @@
-export const track1 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/6173c29f4c68df0a64506906c41ca10cad52b2714865c70bd38ad5d5bc2ea415.m4a'
+// export const track1 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/6173c29f4c68df0a64506906c41ca10cad52b2714865c70bd38ad5d5bc2ea415.m4a'
+//
+// export const track2 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/6ad3c19b4962ad9430a8378b2011e7ac5e1a006260e0dd4a6db0301ffe98c373.m4a'
+//
+// export const track3 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/09e2c84f339ecba76769a0eb7889ed3553cc3f5b7428f80dca4c1b63683b2c0c.m4a'
+//
+// export const track4 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/ddf30ada0e3e865cab9a86cc018f144ea094b1dbc7b6b412454af45f0e751363.m4a'
 
-export const track2 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/6ad3c19b4962ad9430a8378b2011e7ac5e1a006260e0dd4a6db0301ffe98c373.m4a'
 
-export const track3 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/09e2c84f339ecba76769a0eb7889ed3553cc3f5b7428f80dca4c1b63683b2c0c.m4a'
-
-export const track4 = 'https://s3-eu-west-1.amazonaws.com/newsrig/test/tmp/audio/ddf30ada0e3e865cab9a86cc018f144ea094b1dbc7b6b412454af45f0e751363.m4a'
+export const track1 = 'http://127.0.0.1:8080/1-quick.m4a'
+export const track2 = 'http://127.0.0.1:8080/2-jumps.m4a'
+export const track3 = 'http://127.0.0.1:8080/3-lazy.m4a'
